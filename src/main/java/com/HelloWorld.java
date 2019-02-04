fb4f9c22e39bc0439621170b9eff767369c02fa8
Pranav Gour <pranavgour@gmail.com>
2019-02-04 21:06:51 +0530

    commiting helloworld devops build training siemens

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,9 @@
+package com;
+
+public class HelloWorld {
+
+    public void printName(){
+
+    }
+
+}
---
+++ src/test/java/com/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void printhere (){
+
+    }
+
+}
